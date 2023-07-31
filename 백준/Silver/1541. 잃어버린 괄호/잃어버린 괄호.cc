bfd54260312f5ec0830d4c9b7df5347836b8ccf4
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-07-31 14:14:11 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	int lenght{ 0 }, temp{ 0 }, sum{ 0 };
+	bool flag{ false };
+	char str[51];
+
+	cin >> str;
+	lenght = strlen(str);
+
+	for (int i = 0; i <= lenght; i++)
+	{
+		if (str[i] == '+' || str[i] == '-' || i == lenght)
+		{
+			if (flag == false)
+			{
+				sum += temp;
+				temp = 0;
+			}
+			else
+			{
+				sum -= temp;
+				temp = 0;
+			}
+
+			if (str[i] == '-')
+			{
+				flag = true;
+			}
+		}
+		else
+		{
+			temp *= 10;
+			temp += str[i] - '0';
+		}
+	}
+
+	cout << sum;
+
+	return 0;
+}
