6824f0aa76bebe37500016ce2afc7e93e142fbf5
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-10-11 12:53:00 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,45 +1,57 @@
 #include <iostream>
-#include <queue>
-#include <stdio.h>
-#include <cstring> // memset
- 
+#include <deque>
 using namespace std;
- 
-#define MAX_SIZE 100000+1
- 
-int N, K;
-bool visited[MAX_SIZE];
- 
-int bfs() {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-    q.push({ 0, N });
-    visited[N] = true;
-    while (!q.empty()) {
-        int time = q.top().first;
-        int x = q.top().second;
-        q.pop();
- 
-        if (x == K) return time;
- 
-        if (x * 2 < MAX_SIZE && !visited[x * 2]) {
-            q.push({ time, x * 2 });
-            visited[x * 2] = true;
+
+#define MAX 100000+1
+int nodeStart{ 0 }, nodeEnd{ 0 };
+int visited[MAX]{ false };
+
+int BFS()
+{
+    deque<int> dq;
+	dq.push_back(nodeStart);
+	visited[nodeStart] = 1;
+
+    while (false == dq.empty())
+    {
+		int node = dq.front();
+		dq.pop_front();
+
+        if (node == nodeEnd)
+        {
+			return visited[nodeEnd] - 1;
         }
- 
-        if (x + 1 < MAX_SIZE && !visited[x + 1]) {
-            q.push({ time + 1, x + 1 });
-            visited[x + 1] = true;
-        }
- 
-        if (x - 1 >= 0 && !visited[x - 1]) {
-            q.push({ time + 1 , x - 1 });
-            visited[x - 1] = true;
-        }
-    }
+
+		if (node * 2 < MAX && false == visited[node * 2])
+		{
+			dq.push_front(node * 2);
+			visited[node * 2] = visited[node];
+		}
+
+		if (node - 1 >= 0 && false == visited[node - 1])
+		{
+			dq.push_back(node - 1);
+			visited[node - 1] = visited[node] + 1;
+		}
+
+		if (node + 1 < MAX && false == visited[node + 1])
+		{
+			dq.push_back(node + 1);
+			visited[node + 1] = visited[node] + 1;
+		}
+
+	}
+
 }
- 
-int main() {
-    scanf("%d %d", &N, &K);
-    printf("%d\n", bfs());
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> nodeStart >> nodeEnd;
+
+    cout << BFS();
+
     return 0;
 }
