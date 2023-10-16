bb4a2c4d778ec9e136a8ebc7a635c4e795b9233c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-10-16 20:39:08 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 24 ms, Memory: 5620 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,54 +1,65 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
-int n, k;
-bool first = true; // 처음으로 위치가 같은지
-int visit[100003] = { 0 };
-int result = 100003;
-int result2 = 0;
-void bfs(int a, int b){
-    queue<pair<int, int>> q;
-    q.push(make_pair(a,b)); // a: 수빈위치 b: 시간
-    visit[a] = true; // 수빈위치 방문 처리
- 
-    while(!q.empty()){ // 큐가 빌 때까지
-        int x = q.front().first; 
-        int cnt = q.front().second;
-        q.pop();
- 
-        visit[x] = true; // pop 후 방문 처리 
-        if(!first && result == cnt && x == k){
-            // 첫 방식과 다른 방식으로 도달한 방법
-            // 위치가 같고, 걸린 시간도 같은지
-            result2++; // 카운트
-        }
-        if(first && x == k){ //처음으로 위치가 동생하고 같을 때
-            result = cnt; // 최소 시간 저장
-            first = false; // 처음 방문 여부 false로
-            result2++; // 카운트
-        }
- 
-        if(x+1 < 100001 && x+1 >= 0 && !visit[x+1]){
-            // 걷기 +1
-            // 범위 체크 후 다음 점이 방문 된 점인지 확인
-            q.push(make_pair(x + 1, cnt + 1)); 
-        }
-        if(x-1 < 100001 && x-1 >= 0 && !visit[x-1]){
-            // 걷기 -1
-            q.push(make_pair(x - 1, cnt + 1));
-        }
-        if(x*2 < 100001 && x*2 >= 0 && !visit[x*2]){
-            // 순간이동 *2
-            q.push(make_pair(x * 2, cnt + 1));
-        }
-    }
+
+#define MAX 100000
+int nodeStart{ 0 }, nodeEnd{ 0 }, tempTime{ 0 }, result{ 1 };
+int visited[MAX + 1]{ false };
+
+void BFS()
+{
+	queue<pair<int,int>> q;
+	q.push({ nodeStart, 0 });
+	visited[nodeStart] = true;
+
+	while (false == q.empty())
+	{
+		int node = q.front().first;
+		int time = q.front().second;
+		q.pop();
+		visited[node] = true;
+
+		if (node == nodeEnd) 
+		{
+			if (0 == tempTime)
+			{
+				tempTime = time;
+			}
+			else
+			{
+				if (tempTime == time)
+				{
+					++result;
+				}
+			}
+		}
+
+		if (node * 2 <= MAX && visited[node * 2] == false)
+		{
+			q.push({ node * 2, time + 1 });
+		}
+		if (node + 1 <= MAX && visited[node + 1] == false)
+		{
+			q.push({ node + 1, time + 1 });
+		}
+		if (node - 1 >= 0 && visited[node - 1] == false)
+		{
+			q.push({ node - 1, time + 1 });
+		}
+	}
+
 }
-int main(){
-    cin >> n >> k;
- 
-    bfs(n, 0);
-    cout << result << endl; // 걸리는 최소 시간
-    cout << result2 << endl; // 방법 개수
-    return 0;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> nodeStart >> nodeEnd;
+
+	BFS();
+	
+	cout << tempTime << '\n' << result;
+
+	return 0;
 }
