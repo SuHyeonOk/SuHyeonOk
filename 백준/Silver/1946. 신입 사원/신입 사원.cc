6abec3a85c975111264c2e00e49ae313acf3e9d0
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-09 14:59:55 +0900

    [Silver I] Title: 신입 사원, Time: 476 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -14,17 +14,19 @@ int main()
 
     while (T--)
     {
-        vec.clear();
-        int N{ 0 }, a{ 0 }, b{ 0 };
+        int N{ 0 };
         cin >> N;
+
+        vec.clear();
+        vec.resize(N);
 
         for (int i = 0; i < N; i++) // 모든 점수 입력 받기
         {
-            cin >> a >> b;
-            vec.push_back({ a, b });
+            cin >> vec[i].first >> vec[i].second;
         }
 
         sort(vec.begin(), vec.end()); // 서류 등수 순서로 정렬
+
         int result{ 1 }; // 신입  사원은 1명 이상 즉, 1등은 반드시 존재 하기에 1부터 센다.
         int bsetScore{ vec[0].second }; // 서류 1등의 면접 등수
 
