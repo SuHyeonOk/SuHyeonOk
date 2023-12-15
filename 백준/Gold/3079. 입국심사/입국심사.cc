aa74dbb305dfc4107da1046038a60a0d89354661
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-15 15:36:53 +0900

    [Gold V] Title: 입국심사, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.cc
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -5,37 +5,47 @@ using namespace std;
 
 int main()
 {
-	long long n, m;
-	cin >> n >> m;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    long long n{ 0 }, m{ 0 };
+    cin >> n >> m;
 
-	vector<int> vec(n);
-	for (long long i = 0; i < n; i++) 
-	{
-		cin >> vec[i];
-	}
-	sort(vec.begin(), vec.end());
+    vector<int> vec(n);
+    for (long long i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
 
-	long long left{ 1 }, right{ vec[n - 1] * m }, mid{ 0 }, count{ 0 };
+    long long left{ 1 }, right{ vec[n - 1] * m }, mid{ 0 }, count{ 0 };
 
-	while (left <= right)
-	{
-		mid = (left + right) / 2;
-		count = 0;
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+        count = 0;
 
-		for (long long i = 0; i < n; i++)
-		{
-			count += mid / vec[i];
+        for (long long i = 0; i < n; i++)
+        {
+            count += mid / vec[i];
 
-			if (count > m)
-				break;
-		}
+            if (count > m)
+            {
+                break;
+            }
+        }
 
-		if (count >= m) { // 충분하므로 시간을 줄여야한다.
-			right = mid - 1;
-		}
-		else {
-			left = mid + 1;
-		}
-	}
-	cout << left;
+        if (count >= m)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    cout << left;
+
+    return 0;
 }
