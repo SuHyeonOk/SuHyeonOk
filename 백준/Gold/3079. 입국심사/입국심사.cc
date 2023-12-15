67d340569fcaf12585ae314a58e2492057d9fe68
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-15 15:20:07 +0900

    [Gold V] Title: 입국심사, Time: 56 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.cc
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -1,55 +1,45 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-unsigned long long arr[100001];
 
-int main() 
+int main()
 {
-    unsigned long long n{ 0 }, m{ 0 };
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    long long n{ 0 }, m{ 0 };
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) 
+    vector<int> vec(n);
+    for (long long i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin >> vec[i];
     }
 
-    sort(arr, arr + n);
+    sort(vec.begin(), vec.end());
 
-    unsigned long long right = { m * arr[0] };
-    // 심사 시간이 제일 적은 데스크에 모든 사람이 검사 받는 경우를 최대 값으로 잡아줌
-    unsigned long long left{ 1 };
-    unsigned long long mid{ 0 };
-    unsigned long long ans{ 0 };
-    unsigned long long people{ 0 };
+	long long start = 1, end = vec.back() * m;
 
-    while (left <= right)
-    {
-        people = 0;
-        mid = (right + left) / 2;
+	while (start <= end) {
+		long long mid = (start + end) / 2;
+		long long cnt = 0;
 
-        for (int i = 0; i < n; i++) 
-        {
-            people += mid / arr[i]; // mid시간에 검사할 수 있는 인원 수
-        }
+		for (long long i = 0; i < n; i++) {
+			cnt += mid / vec[i];
+			if (cnt > m)
+				break;
+		}
 
-        if (people >= m) 
-        { 
-            // 검사 인원이 주어진 인원이랑 같거나 커야 조건 만족
-            if (ans > mid || ans == 0) 
-            {
-                // 최소시간일때 답 저장
-                ans = mid;
-            }
-            right = mid - 1;
-        }
-        else 
-        {
-            left = mid + 1;
-        }
-    }
-
-    cout << ans;
+		if (cnt >= m) { // 충분하므로 시간을 줄여야한다.
+			end = mid - 1;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+	cout << start;
 
     return 0;
 }
