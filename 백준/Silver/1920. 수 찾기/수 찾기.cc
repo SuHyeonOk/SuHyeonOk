c6e76bac375bf3387f7c30db64fb4989f23b67c0
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-23 20:48:14 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -18,52 +18,13 @@ int main()
     }
     sort(A.begin(), A.end());
 
-    int m{ 0 };
+    int m{ 0 }, target{ 0 };
     cin >> m;
 
-    vector<int> M(m);
     for (int i = 0; i < m; i++)
     {
-        cin >> M[i];
-    }
-
-    bool find{ false };
-    int left{ 0 }, right{ n - 1 }, mid{ 0 }, target{ 0 };
-    for (int i = 0; i < m; i++)
-    {
-        find = false;
-        left = 0;
-        right = n - 1;
-        mid = 0;
-        target = M[i];
-
-        while (left <= right)
-        {
-            mid = (left + right) / 2;
-
-            if (target == A[mid])
-            {
-                find = true;
-                break;
-            }
-            else if (target < A[mid])
-            {
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid + 1;
-            }
-        }
-
-        if (true == find)
-        {
-            cout << 1 << '\n';
-        }
-        else
-        {
-            cout << 0 << '\n';
-        }
+        cin >> target;
+        cout << binary_search(A.begin(), A.end(), target) << '\n';
     }
 
     return 0;
