09e01e356e9b8e06c025108defa3f09140467461
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-27 22:40:14 +0900

    [Bronze III] Title: FBI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string fbi[6];
+    string ans = "";
+    
+    for (int i = 1; i <= 5; i++)
+    {
+        cin >> fbi[i];
+
+        if (fbi[i].find("FBI") == string::npos)
+        {
+            continue;
+        }
+        else
+        {
+            ans += to_string(i);
+        }
+    }
+    
+    if (ans.size() == 0)
+    {
+        cout << "HE GOT AWAY!" << '\n';
+    }
+    else
+    {
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << ' ';
+        }
+    }
+}
