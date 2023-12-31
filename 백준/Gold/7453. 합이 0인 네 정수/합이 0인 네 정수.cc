e7b935e71ebfab9f1cd0b84ba33e36fc994fd2bc
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-12-31 18:01:37 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4040 ms, Memory: 166344 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,46 +1,50 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
-
+#include<algorithm>
 using namespace std;
 
-int N;
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-int arr[4001][4];
-vector<int> A, B;
-long long answer = 0;
-
-int main() {
-
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
+	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) 
-		for (int j = 0; j < 4; j++) cin >> arr[i][j];
+	vector<vector<int>> vec(4001, vector<int>(4));
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> vec[i][j];
+		}
+	}
 
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			A.push_back(arr[i][0] + arr[j][1]);
-			B.push_back(arr[i][2] + arr[j][3]);
-		}	
+	vector<int> A, B;
+	for (int ac = 0; ac < N; ac++) 
+	{
+		for (int bd = 0; bd < N; bd++) 
+		{
+			A.push_back(vec[ac][0] + vec[bd][1]); // [0][0] + [0~n][1]
+			B.push_back(vec[ac][2] + vec[bd][3]);
+		}
 	}
 
 	sort(A.begin(), A.end());
 	sort(B.begin(), B.end());
-	
-	for (int i = 0; i < A.size(); i++) {
-		
-		int B_pre = lower_bound(B.begin(), B.end(), -A[i]) - B.begin();
-		int B_end = upper_bound(B.begin(), B.end(), -A[i]) - B.begin();
 
-		answer += (B_end - B_pre);
+	long long result{ 0 };
+	size_t size{ A.size() };
+	for (size_t i = 0; i < size; i++)
+	{
+		int index = lower_bound(B.begin(), B.end(), -A[i]) - B.begin();
+		int next_index = upper_bound(B.begin(), B.end(), -A[i]) - B.begin();
+
+		result += (next_index - index);
 	}
 
-	cout << answer << endl;
-	
+	cout << result << endl;
+
 	return 0;
 
 }
