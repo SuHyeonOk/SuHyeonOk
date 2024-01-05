8969a403613479c1bff050b5a0c51b41ac7db67c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-01-05 17:12:06 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+string arr[1000];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    size_t n{ 0 };
+    cin >> n;
+
+    string input{ "" };
+    map<string, int> m;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> input;
+        
+        map<string, int>::iterator it = m.find(input);
+        if (m.end() == it) // 같은 값이 없는 경우
+        {
+            m.insert({ input, 1 });
+        }
+        else // 같은 값이 있는 경우
+        {
+            ++it->second;
+        }
+    }
+
+    int max_value{ 0 };
+    for (map<string, int>::iterator it = m.begin(); it != m.end(); ++it)
+    {
+        if (max_value < it->second)
+        {
+            max_value = it->second;
+        }
+    }
+
+    for (map<string, int>::iterator it = m.begin(); it != m.end(); ++it)
+    {
+        if (max_value == it->second)
+        {
+            cout << it->first << '\n';
+            break;
+        }
+    }
+
+    return 0;
+}
