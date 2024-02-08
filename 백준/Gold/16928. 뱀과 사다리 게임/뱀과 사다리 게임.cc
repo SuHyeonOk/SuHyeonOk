6755b4930a508b400abb0cb203e49cdd7ae6846b
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-02-08 12:50:28 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#define MAX 101
+using namespace std;
+
+/*
+-1~6 숫자는 자유롭게 이동
+- 뱀은 무조건 안 밟는게 좋을까
+*/
+
+int N, M;
+int map[MAX];
+bool visited[MAX];
+
+int BFS()
+{
+    queue<pair<int, int>>q;
+    q.push({ 1, 0 });
+    visited[1] = true;
+
+    while (false == q.empty())
+    {
+        int X = q.front().first;
+        int Count = q.front().second + 1;
+        q.pop();
+
+        for (int i = 1; i <= 6; i++)
+        {
+            int NX = X + i;
+
+            if (NX == 100)
+            {
+                return Count;
+            }
+            else if (NX < 100)
+            {
+                while (0 != map[NX])
+                {
+                    NX = map[NX];
+                }
+
+                if (false == visited[NX])
+                {
+                    q.push({ NX, Count });
+                    visited[NX] = true;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N >> M;
+
+    int x = 0, y = 0;
+    for (int i = 0; i < N + M; i++)
+    {
+        cin >> x >> y;
+        map[x] = y;
+    }
+
+    cout << BFS();
+
+    return 0;
+}
