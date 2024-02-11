fcda11658f4c96242bc18b409f5fcd5b46ff0756
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-02-11 21:30:20 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int A = 0, B = 0;
+    cin >> A >> B;
+    cout << A * B;
+
+    return 0;
+}
