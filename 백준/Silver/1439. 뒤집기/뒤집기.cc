7fa9a2e0107b7f3517a3ac9c76f676bc7e01912c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-02-19 23:04:32 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    string s = "";
+    int result = 0;
+    cin >> s;
+
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]!=s[i+1]) 
+        {
+            result++;
+        }
+    }
+        
+    if(false == result) 
+    {
+        cout << result;
+    }
+    else
+    {
+        cout << result/2;
+    }
+    
+    return 0;
+}
