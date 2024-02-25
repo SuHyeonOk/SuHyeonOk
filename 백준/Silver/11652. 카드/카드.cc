8043725513cc516b582f1ca44b2331f325f7dcba
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-02-25 22:01:35 +0900

    [Silver IV] Title: 카드, Time: 28 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+long long card[1000000];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n = 0;
+    cin >> n;
+    
+	for (int i = 0; i < n; i++)
+    {
+        cin >> card[i];
+    }
+    
+	sort(card, card + n);
+
+	int cnt = 1, max = 0, index = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (card[i] == card[i + 1])
+		{
+			cnt++;
+			if (cnt > max)
+			{
+				max = cnt;
+				index = i;
+			}
+		}
+		else
+        {
+            cnt = 1;
+        }
+	}
+	cout << card[index] << '\n';
+	
+}
