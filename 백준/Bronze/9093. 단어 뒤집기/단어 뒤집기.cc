8e97a9eddf258630d19fcc698af3370766f5a982
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-03-29 15:26:52 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -1,42 +1,45 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int T = 0;
-    cin >> T;
-    cin.ignore(); // 입력 버퍼 비우기 (T 입력 후 남은 '\n' 제거)
+    int TestCase = 0;
+    cin >> TestCase;
+    cin.ignore();
 
-    stack<char> Ss;
-
-    while (T--)
+    string Str = "", Temp = "", Result = "";
+    while(TestCase--)
     {
-        string str;
-        getline(cin, str);  // 한 줄 읽어오기
-        str += ' '; // 마지막 확인용
-
-        for (int i = 0; i < str.length(); i++) 
+        getline(cin, Str);
+      
+        size_t Length = Str.length();
+        for (size_t i = 0; i < Length; i++)
         {
-            if (str[i] == ' ') 
+            if (' ' == Str[i]) // 공백
             {
-                while (false == Ss.empty()) 
-                {
-                    cout << Ss.top();
-                    Ss.pop();
-                }
-                cout << ' ';
+                reverse(Temp.begin(), Temp.end());
+                Result += Temp;
+                Result += ' ';
+                Temp = "";
             }
-            else
+            else // 문자
             {
-                Ss.push(str[i]);  // 공백 아닐 경우 push
+                Temp += Str[i];
             }
         }
-        cout << "\n";
+    
+        reverse(Temp.begin(), Temp.end());
+        Result += Temp;
+
+        cout << Result << '\n';
+
+        Result = "";
+        Temp = "";
     }
 
     return 0;
