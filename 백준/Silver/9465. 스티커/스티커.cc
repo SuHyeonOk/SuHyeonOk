3f21f8e4472fc23752d3a04d8221170cd3014c78
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-04-03 14:44:36 +0900

    [Silver I] Title: 스티커, Time: 80 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,41 +1,47 @@
 #include <iostream>
-#include <cstdio>
- 
+#define MAX 100000
 using namespace std;
- 
-const int MAXN = 100000;
- 
-int buf[2][MAXN + 1] = { 0, };
-int dp[2][MAXN + 1] = { 0, };
- 
-int max(int a, int b) { if (a > b) return a; return b; }
- 
-int bottomUp(int n) {
-    for (int i = 0; i < n; i++) 
-        scanf("%d", &buf[0][i]);
- 
-    for (int i = 0; i < n; i++)
-        scanf("%d", &buf[1][i]);
- 
-    dp[0][0] = buf[0][0];
-    dp[1][0] = buf[1][0];
-    dp[0][1] = buf[0][1] + dp[1][0];
-    dp[1][1] = buf[1][1] + dp[0][0];
- 
-    for (int i = 2; i < n; i++) {
-        dp[0][i] = buf[0][i] + max(dp[1][i - 1], dp[1][i - 2]);
-        dp[1][i] = buf[1][i] + max(dp[0][i - 1], dp[0][i - 2]);
+
+int sticker[2][MAX], dp[2][MAX];
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int T = 0, N = 0;
+    cin >> T;
+
+    while (T--) 
+    {
+        cin >> N;
+
+        // 스티커 첫번째 줄, 두번째 줄 입력 받기
+        for (int i = 0; i < N; i++)
+        {
+            cin >> sticker[0][i];
+        }
+        for (int i = 0; i < N; i++)
+        {
+            cin >> sticker[1][i];
+        }
+
+        // 초기값 세팅
+        dp[0][0] = sticker[0][0];
+        dp[1][0] = sticker[1][0];
+        dp[0][1] = sticker[0][1] + dp[1][0];
+        dp[1][1] = sticker[1][1] + dp[0][0];
+
+        // 2부터 시작하는 dp 점화식
+        for (int i = 2; i < N; i++) 
+        {
+            dp[0][i] = sticker[0][i] + max(dp[1][i - 1], dp[1][i - 2]);
+            dp[1][i] = sticker[1][i] + max(dp[0][i - 1], dp[0][i - 2]);
+        }
+
+        // 둘 중에 더 큰 값 출력
+        cout << max(dp[0][N - 1], dp[1][N - 1]) << '\n';
     }
- 
-    return max(dp[0][n - 1], dp[1][n - 1]);
-}
- 
-int main() {
-    int t, n;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d", &n);
-        printf("%d\n", bottomUp(n));
-    }
+
     return 0;
 }
