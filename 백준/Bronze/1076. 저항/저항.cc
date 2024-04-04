c793de669f85050f554a2bacff9c687fe9deb500
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-04-04 20:32:34 +0900

    [Bronze II] Title: 저항, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long answer;
+string a, b, c;
+map<string, int> valuemap;
+vector<string> stv{"black", "brown", "red", "orange", "yellow",
+                   "green", "blue", "violet", "grey", "white"};
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  for(int i = 0; i < 10; i++){
+    valuemap.insert({stv[i], i});
+  }
+  cin >> a >> b >> c;
+
+  if(valuemap[a] != 0) answer = 10 * valuemap[a];
+  answer += valuemap[b];
+  answer *= pow(10, valuemap[c]);
+  cout << answer;
+
+  return 0;
+}
