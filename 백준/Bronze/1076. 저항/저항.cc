705e0c064746326f7ed6a25b1ab5df7e9abd139c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-04-06 16:29:34 +0900

    [Bronze II] Title: 저항, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.cc
+++ 백준/Bronze/1076. 저항/저항.cc
@@ -1,34 +1,66 @@
 #include <iostream>
-#include <map>
 #include <string>
 using namespace std;
+
+string Table(string _Input)
+{
+    if (_Input == "black")
+    {
+        return "0";
+    }
+    else if (_Input == "brown")
+    {
+        return "1";
+    }
+    else if (_Input == "red")
+    {
+        return "2";
+    }
+    else if (_Input == "orange")
+    {
+        return "3";
+    }
+    else if (_Input == "yellow")
+    {
+        return "4";
+    }
+    else if (_Input == "green")
+    {
+        return "5";
+    }
+    else if (_Input == "blue")
+    {
+        return "6";
+    }
+    else if (_Input == "violet")
+    {
+        return "7";
+    }
+    else if (_Input == "grey")
+    {
+        return "8";
+    }
+    else if (_Input == "white")
+    {
+        return "9";
+    }
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    map<string, string> m;
-    m.insert({ "black", "0"});
-    m.insert({ "brown", "1"});
-    m.insert({ "red", "2"});
-    m.insert({ "orange", "3"});
-    m.insert({ "yellow", "4"});
-    m.insert({ "green", "5"});
-    m.insert({ "blue", "6"});
-    m.insert({ "violet", "7"});
-    m.insert({ "grey", "8"});
-    m.insert({ "white", "9"});
-
-    string Input = "white";
-    cin >> Input;
-    string Result = m[Input];
+    string Input = "", Result = "";
+    
+    for (size_t i = 0; i < 2; i++)
+    {
+        cin >> Input;
+        Result += Table(Input);
+    }
 
     cin >> Input;
-    Result += m[Input];
-
-    cin >> Input;
-    int Num = stoi(m[Input]);
+    int Num = stoi(Table(Input));
     for (int i = 0; i < Num; ++i)
     {
         Result += "0";
