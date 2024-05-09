585e8febe4730c534e4b27876e8770b1bcb78139
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-05-09 16:01:09 +0900

    [Gold V] Title: 센서, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int N, K;
+vector<int> v;
+vector<int> v2;
+int main(void)
+{
+
+	cin >> N;
+	cin >> K;
+
+	if (K >= N)
+	{
+		cout <<"0";
+		return 0;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N - 1; i++)
+		v2.push_back(v[i + 1] - v[i]);
+
+	sort(v2.begin(), v2.end());
+
+	int res = 0;
+	for (int i = 0; i < v2.size() - (K - 1); i++)
+		res += v2[i];
+	cout << res;
+}
