3d6457137d7e3d1152812386792d44b721dcc665
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-05-22 21:35:55 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,29 +1,37 @@
-#include<iostream>
- 
+#include <iostream>
 using namespace std;
- 
-int divide(int x, int y)
+
+int gcd(int a, int b) // 최대공약수 (유클리드 호제법)
 {
-    if (x % y == 0)
-        return y;
-    else
-        return divide(y, x % y);
+	int r = a % b;
+	if (r == 0) 
+	{
+		return b;
+	}
+	else 
+	{
+		return gcd(b, r);
+	}
 }
- 
-int main()
+
+int main() 
 {
-    int T;
-    int A, B;
-    cin >> T;
- 
-    for (int i = 0; i < T; i++)
-    {
-        cin >> A >> B;
-        if (A >= B)
-        {
-            cout << A * B / divide(A, B) << "\n";
-        }
-        else
-            cout << A * B / divide(B, A) << "\n"; 
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int T = 0;
+	cin >> T;
+
+	int Ans = 0;
+	int A = 0, B = 0;
+	for (int i = 0; i < T; i++) 
+	{
+		cin >> A >> B;
+
+		// 최소공배수 = (A x B) / 최대공약수
+		Ans = (A * B) / (gcd(A, B));
+		cout << Ans << '\n';
+	}
+
+	return 0;
 }
