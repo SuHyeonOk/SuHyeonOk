741358c5c4248ee7faad9fc3d4585dc16da00eea
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-05-27 17:21:40 +0900

    [Silver II] Title: 꽃길, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14620. 꽃길/꽃길.cc
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#define MAX 10
+using namespace std;
+
+int N;
+int Result = INT_MAX;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
+
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
+
+void DFS(int sum, int cnt) 
+{
+	if (cnt == 3) 
+	{
+		Result = min(Result, sum); 
+		return;
+	}
+
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			int check = 0;
+			int nx = 0, ny = 0;
+
+			for (int k = 0; k < 4; k++) 
+			{
+				nx = j + dx[k];
+				ny = i + dy[k];
+
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+				{
+					continue;
+				}
+
+				if (true == visited[ny][nx])
+				{
+					continue;
+				}
+
+				check++;
+			}
+
+			if (check == 4) 
+			{
+				int ssum = map[i][j];
+				for (int k = 0; k < 4; k++) 
+				{
+					nx = j + dx[k];
+					ny = i + dy[k];
+					visited[ny][nx] = true;
+					ssum += map[ny][nx];
+				}
+
+				visited[i][j] = true;
+				sum += ssum;
+				cnt++;
+				DFS(sum, cnt);
+				sum -= ssum;
+				cnt--;
+
+				for (int k = 0; k < 4; k++) 
+				{
+					nx = j + dx[k];
+					ny = i + dy[k];
+					visited[ny][nx] = 0;
+				}
+
+				visited[i][j] = false;
+			}
+		}
+	}
+
+}
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			cin >> map[i][j];
+		}
+	}
+
+	DFS(0, 0);
+
+	cout << Result;
+
+	return 0;
+}
