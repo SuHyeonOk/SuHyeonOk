fc2a5d4a470b5ee26098542e307c919d9491ef89
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-05-29 23:56:29 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N;
+int alphabet[26];
+string s = "abcdefghijklmnopqrstuvwxyz";
+
+int main() 
+{    
+	cin >> N;
+	for (int i = 0; i < N; i++) 
+    {
+		string input;
+		cin >> input;
+		alphabet[input[0] - 'a']++;
+	}
+    
+	bool predaja = true;
+	for (int i = 0; i < 26; i++) 
+    {
+		if (alphabet[i] >= 5) 
+        {
+			cout << s[i];
+			predaja = false;
+		}
+	}
+    
+	if (true == predaja)
+    {
+         cout << "PREDAJA";
+    }
+    
+    return 0;
+}
