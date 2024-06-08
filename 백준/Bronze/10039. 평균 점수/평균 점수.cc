8900848b71b84df032008ecc9d76f92daa11fea2
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-06-08 21:26:03 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int total = 0, tmp = 0;
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> tmp;
+        if(tmp < 40)
+        {
+            tmp = 40;
+        }
+        total += tmp;
+    }
+    
+    cout << total/5;
+    
+    return 0;
+}
