d102a7e8da445d5a815988acd3bad061de60c169
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-07-16 14:40:26 +0900

    [Bronze III] Title: 소음, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2935. 소음/소음.cc
+++ 백준/Bronze/2935. 소음/소음.cc
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-int main() 
+int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -11,30 +11,31 @@ int main()
     char oper = ' ';
     cin >> a >> oper >> b;
 
-    int len_a = a.size();
-    int len_b = b.size();
-    switch (oper) 
+    int len_a = a.length();
+    int len_b = b.length();
+
+    switch (oper)
     {
     case '+':
-        if (len_a >= len_b) 
+        if (len_a >= len_b)
         {
             a[len_a - len_b] = a[len_a - len_b] + 1;
             cout << a;
-        }        
-        else 
+        }
+        else
         {
             b[len_b - len_a] = '1';
             cout << b;
         }
         break;
     case '*':
-        for (int i = 1; i < len_b; ++i) 
+        for (int i = 1; i < len_b; ++i)
         {
             a += '0';
         }
         cout << a;
         break;
     }
-    
+
     return 0;
 }
