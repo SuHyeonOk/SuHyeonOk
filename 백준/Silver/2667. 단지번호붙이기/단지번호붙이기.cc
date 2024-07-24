ba80815fae7f802ab6be684c65c7b350f712294c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-07-24 14:53:02 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define MAX 25
+using namespace std;
+
+int N;
+char arr[MAX][MAX];
+bool visited[MAX][MAX];
+vector<int> result;
+int dx[4]{ 0, 0, -1, 1 };
+int dy[4]{ -1, 1, 0, 0 };
+
+void BFS(int _x, int _y)
+{
+    queue<pair<int, int>> q;
+    q.push({_x, _y});
+
+    int Move = 1;
+    visited[_y][_x] = true;
+
+    while (false == q.empty())
+    {
+        int X = q.front().first;
+        int Y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; ++i)
+        {
+            int NX = X + dx[i];
+            int NY = Y + dy[i];
+
+            if (0 > NX || N <= NX || 0 > NY || N <= NY)
+            {
+                continue;
+            }
+
+            if (true == visited[NY][NX])
+            {
+                continue;
+            }
+
+            if ('0' == arr[NY][NX])
+            {
+                continue;
+            }
+
+            visited[NY][NX] = true;
+            q.push({ NX, NY });
+            ++Move;
+        }
+    }
+
+    result.push_back(Move);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < N; ++x)
+        {
+            cin >> arr[y][x];
+        }
+    }
+
+    int Total = 0;
+    for (int y = 0; y < N; ++y)
+    {
+        for (int x = 0; x < N; ++x)
+        {
+            if (false == visited[y][x] && '1' == arr[y][x])
+            {
+                ++Total;
+                BFS(x, y);
+            }
+        }
+    }
+
+    cout << Total << '\n';
+    sort(result.begin(), result.end());
+    Total = result.size();
+    for (int i = 0; i < Total; ++i)
+    {
+        cout << result[i] << '\n';
+    }
+
+    return 0;
+}
