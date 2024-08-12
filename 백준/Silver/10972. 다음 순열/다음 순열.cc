08cd8fa315b5dc14efae7596e75741a2de367726
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-08-12 11:46:44 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -1,33 +1,66 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() 
-{	
-	vector<int> arr;
-	int N = 0;
-	cin >> N;
-
-	for (int i = 0; i < N; i++) 
+bool next_permutation(vector<int>& a, int n)
+{
+    int i = n - 1;
+    while (i > 0 && a[i - 1] >= a[i])
     {
-		int x;
-		cin >> x;
-		arr.push_back(x);
-	}
+        i -= 1; // 뒤에서부터 어디까지 내림차순인지 찾아보기
+    }
 
-	if (next_permutation(arr.begin(), arr.end())) 
+    // while문을 빠져나왔을때의 i가 내림차순의 시작하는 부분
+    if (i <= 0)
     {
-		for (auto element : arr)
+        return false;
+    }
+
+    int j = n - 1;
+    while (a[j] <= a[i - 1])
+    {
+        j -= 1; // 내림차순 바로 앞의 숫자보다 처음으로 큰 j를 뒤에서부터 찾아보기
+    }
+
+    swap(a[i - 1], a[j]);
+    j = n - 1; // 순열의 마지막 자리
+
+    while (i < j) // 내림차순의 시작부분부터 마지막 짜리까지 뒤집어준다.
+    {
+        swap(a[i], a[j]);
+        i += 1;
+        j -= 1;
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+
+    vector<int> vec(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> vec[i];
+    }
+
+    if (next_permutation(vec, N))
+    {
+        for (int i = 0; i < N; ++i)
         {
-            cout << element << " ";
+            cout << vec[i] << ' ';
         }
-		cout << endl;
-	}
-	else 
+        cout << '\n';
+    }
+    else
     {
-		cout << -1;
-	}
+        cout << -1;
+    }
 
-	return 0;
+    return 0;
 }
