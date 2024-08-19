1b8872038dd77624a19d7bf8775a90c823849079
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-08-19 13:59:24 +0900

    [Silver I] Title: 후보 추천하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
@@ -3,64 +3,76 @@
 #include <algorithm>
 using namespace std;
 
-typedef struct tagInformation {
+typedef struct tagInformation
+{
     int Student = 0;
     int Voting = 0;
-    int Time = 0;  // 추천 받은 시간을 저장하는 변수
-} INFO;
+}INFO;
 
-bool Compare(INFO& A, INFO& B) {
+bool Compaie(INFO& A, INFO& B)
+{
     return A.Student < B.Student;
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     int N = 0, M = 0;
     cin >> N >> M;
 
-    int Recommendation = 0;
     deque<INFO> d;
-    int time = 0;
-
-    for (int i = 0; i < M; ++i) {
+    int Recommendation = 0;
+    for (int i = 0; i < M; ++i)
+    {
         cin >> Recommendation;
-        time++;  // 시간 증가
 
         // 이전에 추천 받은 경우가 있는지 확인
         bool Find = false;
-        for (auto& info : d) {
-            if (Recommendation == info.Student) {
-                ++info.Voting;
+        deque<INFO>::iterator it = d.begin();
+        for (it = d.begin(); it != d.end(); ++it)
+        {
+            if (Recommendation == it->Student)
+            {
+                ++it->Voting;
                 Find = true;
                 break;
             }
         }
 
-        if (Find) {
+        if (true == Find)
+        {
             continue;
         }
 
-        if (d.size() < N) { // 액자가 남는 경우
-            d.push_back({ Recommendation, 1, time });
+        if (d.size() < N) // 액자가 남는 경우
+        {
+            // 처음 추천 받는 경우
+            d.push_back({ Recommendation, 1 });
         }
-        else { // 액자가 남지 않아서 후보 제외하고 추가하기
-            // 투표가 가장 적은 학생을 찾는다. 동일 투표수일 경우 더 오래된 학생을 찾는다.
-            auto Exception = min_element(d.begin(), d.end(), [](INFO& A, INFO& B) {
-                if (A.Voting == B.Voting)
-                    return A.Time < B.Time;
-                return A.Voting < B.Voting;
-                });
+        else // 액자가 남지 않아서 후보 제외 하고 추가하기
+        {
+            int Min = 1001;
+            deque<INFO>::iterator Exception = d.begin();
+            for (it = d.begin(); it != d.end(); ++it)
+            {
+                if (Min > it->Voting) // 투표가 가장 적은 학생
+                {
+                    Min = it->Voting;
+                    Exception = it;
+                }
+            }
 
             d.erase(Exception);
-            d.push_back({ Recommendation, 1, time });
+            d.push_back({ Recommendation, 1 });
         }
     }
 
-    sort(d.begin(), d.end(), Compare);
-    for (auto& info : d) {
-        cout << info.Student << ' ';
+    sort(d.begin(), d.end(), Compaie);
+    for (deque<INFO>::iterator it = d.begin(); it != d.end(); ++it)
+    {
+        cout << it->Student << ' ';
     }
 
     return 0;
