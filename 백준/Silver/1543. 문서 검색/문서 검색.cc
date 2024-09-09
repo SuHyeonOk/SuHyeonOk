9ac9dd5e0e36544d89b647aebe17f90cdf24880f
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-09-09 17:29:47 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string Document = "", Word = "";
+    getline(cin, Document);
+    getline(cin, Word);
+
+    size_t DocumentSize = Document.size();
+    size_t WordSize = Word.size();
+
+    int Result = 0;
+    for (size_t i = 0; i < DocumentSize;)
+    {
+        string Temp = Document.substr(i, WordSize);
+        if (0 == Word.compare(Temp))
+        {
+            ++Result;
+            i += WordSize;
+        }
+        else
+        {
+            ++i;
+        }
+    }
+
+    cout << Result;
+
+    return 0;
+}
