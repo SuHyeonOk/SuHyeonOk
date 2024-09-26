14c72a73bd0e4832d9afab559b1088cc7f202ca3
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-09-26 12:35:37 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cout << "비와이" << '\n';
+
+    return 0;
+}
