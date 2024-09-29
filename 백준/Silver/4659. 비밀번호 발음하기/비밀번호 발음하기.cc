1ae82513276b7c4f611b7584dc5ae8d32541d438
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-09-29 15:48:27 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,77 +1,65 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
- 
+
 int main()
 {
-    string N = "";
-    char vowel[5] = {'a','e','i','o','u'};
-    
-    while(true)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string Input = "";
+    while (true)
     {
-        cin >> N;
-        int a = 0;
-        int v = 0;
-        int c = 0;
-        
-        if(N=="end")
+        cin >> Input;
+
+        if (Input == "end")
         {
             break;
         }
-        for(int i=0; i<N.length(); i++)
+
+        int Vowel = 0, Consonant = 0; // 모음 자음
+        bool Find = false;
+        char Temp = ' ';
+        string Print = "is acceptable.";
+        size_t Length = Input.length();
+        for (size_t i = 0; i < Length; ++i)
         {
-            for(int j=0; j<5; j++)
-            {   // 1번조건
-                if(N[i]==vowel[j])
-                {
-                    a = 1; break;
-                }
-            }
-        }
-        if(!a)
-        { 
-            cout << "<" << N << ">" << " is not acceptable." << endl;
-        }
-        else
-        {
-            for(int i=0; i<N.length(); i++)
+            if (Vowel >= 3 || Consonant >= 3 || Temp == Input[i])
             {
-                int b = 1;
-                if(N[i]==N[i+1] && N[i]!='o' && N[i]!='e')
-                {  // 3번조건
-                    a = 0; break;
-                }
-                for(int j=0; j<5; j++)
-                {  //2번조건
-                    if(N[i]==vowel[j])
-                    {
-                        b = 0;  // v이면 0, c이면 1
-                    }
-                }
-                if(b==0)
-                { 
-                    v++; 
-                    c=0;
-                }
-                else
-                { 
-                    c++; v=0; 
-                }
-                if(c==3 || v==3)
-                {
-                    a = 0; 
-                    break;
-                }
+                Print = "is not acceptable.";
+                break;
             }
-            if(!a)
-            { 
-                cout << "<" << N << ">" << " is not acceptable." << endl;
+
+            if (Input[i] == 'e' || Input[i] == 'o' || Input[i] == 'a' || Input[i] == 'i' || Input[i] == 'u')
+            {
+                if (Length >= i + 1 && Input[i + 1] == 'e' || Input[i + 1] == 'o')
+                {
+                    ++i;
+                    ++Vowel;
+                }
+                Find = true;
+                Consonant = 0;
+                ++Vowel;
             }
             else
-            { 
-                cout << "<" << N << ">" << " is acceptable." << endl;
+            {
+                Vowel = 0;
+                ++Consonant;
+            }
+
+            Temp = Input[i];
+            if (Temp == 'e' || Temp == 'o')
+            {
+                Temp = ' ';
             }
         }
+
+        if (false == Find || Vowel >= 3 || Consonant >= 3)
+        {
+            Print = "is not acceptable.";
+        }
+
+        cout << '<' << Input << "> " << Print << '\n';
     }
+
     return 0;
 }
