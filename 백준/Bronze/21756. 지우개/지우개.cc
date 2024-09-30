85a064f50f26808aa1e888745a587be300ad5d14
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-09-30 11:49:24 +0900

    [Bronze II] Title: 지우개, Time: 0 ms, Memory: 2020 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/21756. 지우개/지우개.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+
+    deque<int> dq;
+    for (int i = 1; i <= N; ++i)
+    {
+        dq.push_back(i);
+    }
+    
+    int Count = 1;
+    while (1 < dq.size())
+    {
+        if (Count % 2 != 0) // 홀수
+        {
+            dq.pop_front();
+        }
+        else
+        {
+            int Temp = dq.front();
+            dq.pop_front();
+            dq.push_back(Temp);
+        }
+
+        ++Count;
+        if (N <= Count)
+        {
+            --N;
+            Count = N;
+        }
+    }
+
+    cout << dq.front();
+
+    return 0;
+}
