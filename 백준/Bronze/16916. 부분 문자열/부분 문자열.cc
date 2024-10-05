236e063c6af0d65d53019f0e6616b187fce7dcd3
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-05 10:13:17 +0900

    [Bronze II] Title: 부분 문자열, Time: 12 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -1,20 +1,72 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
 using namespace std;
 
-int main() 
+// P의 접두사 테이블을 계산하는 함수
+vector<int> makeLPS(const string& P)
+{
+    int m = P.size();
+    vector<int> lps(m, 0);
+    int j = 0;
+
+    for (int i = 1; i < m; ++i)
+    {
+        while (j > 0 && P[i] != P[j])
+        {
+            j = lps[j - 1];
+        }
+
+        if (P[i] == P[j])
+        {
+            lps[i] = ++j;
+        }
+    }
+
+    return lps;
+}
+
+// KMP 알고리즘으로 문자열 매칭을 찾는 함수
+bool KMP(const string& S, const string& P)
+{
+    vector<int> lps = makeLPS(P);
+    int n = S.size();
+    int m = P.size();
+    int j = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        while (j > 0 && S[i] != P[j])
+        {
+            j = lps[j - 1];
+        }
+
+        if (S[i] == P[j])
+        {
+            if (j == m - 1)
+            {
+                return true;  // 패턴을 찾음
+            }
+
+            ++j;
+        }
+    }
+
+    return false;  // 패턴을 찾지 못함
+}
+
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    string Str = "", Temp = "";
-    cin >> Str >> Temp;
+    string S, P;
+    cin >> S >> P;
 
-    if (strstr(Str.c_str(), Temp.c_str()) != NULL) 
+    if (KMP(S, P) == true)
     {
         cout << 1;
     }
-    else 
+    else
     {
         cout << 0;
     }
