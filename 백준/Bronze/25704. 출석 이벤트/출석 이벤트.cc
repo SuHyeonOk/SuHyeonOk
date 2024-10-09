bad49160bb69989c690643bd304bd15462b37782
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-09 23:50:13 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N = 0, P = 0; 
+    cin >> N >> P;
+    
+	int ans = P;
+	if (N >= 5) 
+    {
+        ans = min(ans, P - 500);
+    }
+	if (N >= 10) 
+    {
+        ans = min(ans, P / 10 * 9);
+    }
+	if (N >= 15) 
+    {
+        ans = min(ans, P - 2000);
+    }
+	if (N >= 20) 
+    {
+        ans = min(ans, P / 4 * 3);
+    }
+
+	cout << max(ans, 0);
+
+    return 0;
+}
