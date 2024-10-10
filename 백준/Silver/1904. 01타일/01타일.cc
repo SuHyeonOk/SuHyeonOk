7d0ecb1402c44df4111bf45f5ace83a71559c40c
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-10 16:29:08 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+long long DP[1000001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+
+    DP[1] = 1;
+    DP[2] = 2;
+
+    for (int i = 3; i <= N; ++i)
+    {
+        DP[i] = (DP[i - 1] + DP[i - 2]) % 15746;
+    }
+
+    cout << DP[N] % 15746;
+
+    return 0;
+}
