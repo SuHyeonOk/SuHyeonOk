e331ba8e3d8b38702931908d50e5ce4460726477
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-14 12:10:20 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N = 0, K = 0;
+    cin >> N >> K;
+
+    bool visited[1001]{ false }; 
+    for (int i = 2; i <= N; ++i)
+    {
+        if (false == visited[i])
+        {
+            for (int j = i; j <= N; j += i)
+            {
+                if (false == visited[j])
+                {
+                    visited[j] = true;  
+                    --K;
+
+                    if (K == 0)    
+                    {
+                        cout << j;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
