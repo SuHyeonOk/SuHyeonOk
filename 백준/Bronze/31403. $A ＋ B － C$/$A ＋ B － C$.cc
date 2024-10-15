1560dca9717a5a2184b8d33c31fff1fdb1b93cb4
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-15 09:43:38 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    string A = "", B = "", C = "";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int A = 0, B = 0, C = 0;
     cin >> A >> B >> C;
-    
-    cout << stoi(A) + stoi(B) - stoi(C) << "\n";
-    cout << stoi(A + B) - stoi(C);
-    
+
+    cout << A + B - C << '\n';
+
+    string Temp = to_string(A) + to_string(B);
+    int Result = stoi(Temp) - C;
+    cout << Result;
+
     return 0;
 }
