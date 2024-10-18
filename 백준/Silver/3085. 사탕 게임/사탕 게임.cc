96171a1df48e343b4d7810cbf87dfa43066cc590
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-18 13:14:14 +0900

    [Silver II] Title: 사탕 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.cc
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -2,115 +2,111 @@
 #include <algorithm>
 using namespace std;
 
-int N;
-char candy[50][50];
+char anipang[51][51];
 
-int CountFunction()
-{
-    int Max = 0, Count = 1;
-    
-    // 행
-    int Index = 0;
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < N - 1; ++x)
-        {
-            if (0 == Index)
-            {
-                Index = x;
-            }
-
-            if (candy[y][Index] == candy[y][x + 1])
-            {
-                ++Count;
-            }
-            else
-            {
-                Max = max(Max, Count);
-                Index = 0;
-                Count = 1;
-                continue;
-            }
-        }
-
-        Max = max(Max, Count);
-        Count = 1;
-        Index = 0;
-    }
-
-    // 열
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < N - 1; ++x)
-        {
-            if (candy[x][y] == candy[x + 1][y])
-            {
-                ++Count;
-            }
-            else
-            {
-                Max = max(Max, Count);
-                Count = 1;
-                continue;
-            }
-        }
-
-        Max = max(Max, Count);
-        Count = 1;
-    }
-
-    return Max;
-}
+//이미 한 번 바꿨을 경우와, 한 번도 안 바꿨을 경우를 고려해준다.
+int arr[2];
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    cin >> N;
+	int N;
+	cin >> N;
 
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < N; ++x)
-        {
-            cin >> candy[y][x];
-        }
-    }
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			char c;
+			cin >> c;
+			anipang[i][j] = c;
+		}
+	}
 
-    int Max = 0;
-    Max = max(Max, CountFunction());
+	int M = 1;
 
-    char Temp = ' ';
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < N - 1; ++x)
-        {
-            // 행
-            Temp = candy[y][x];
-            candy[y][x] = candy[y][x + 1];
-            candy[y][x + 1] = Temp;
-            Max = max(Max, CountFunction());
-            Temp = candy[y][x];
-            candy[y][x] = candy[y][x + 1];
-            candy[y][x + 1] = Temp;
+	//가로에 대해서 먼저 조사
 
-            // 열
-            Temp = candy[x][y];
-            candy[x][y] = candy[x + 1][y];
-            candy[x + 1][y] = Temp;
-            Max = max(Max, CountFunction());
-            Temp = candy[x][y];
-            candy[x][y] = candy[x + 1][y];
-            candy[x + 1][y] = Temp;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			arr[0] = 1;
+			arr[1] = 1;
+			char basicWord = anipang[i][j];
+			for (int k = j + 1; k <= N; k++)
+			{
+				if (basicWord == anipang[i][k])
+				{
+					arr[0]++;
+					arr[1]++;
+				}
+				else if (basicWord == anipang[i + 1][k] || basicWord == anipang[i - 1][k])
+				{
+					arr[1] = arr[0] + 1;
+					arr[0] = 0;
+				}
+				else if (basicWord == anipang[i][k - 1] && anipang[i][k - 1] == anipang[i][k + 1])
+				{
+					arr[1] = arr[0] + 1;
+					M = max({ M,arr[0], arr[1] });
+					arr[1] = 1;
+					arr[0] = 0;
+				}
+				else
+				{
+					//p p p c p p p p
+					//c c p c c p
+					arr[1] = 0;
+					arr[0] = 0;
+				}
+				M = max({ M,arr[0], arr[1] });
+			}
+		}
+	}
 
-            if (Max == 4)
-            {
-                int a = 0;
-            }
-        }
-    }
-    
-    cout << Max;
+	//세로에 대한 조사
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			arr[0] = 1;
+			arr[1] = 1;
+			char basicWord = anipang[j][i];
+			for (int k = j + 1; k <= N; k++)
+			{
+				if (basicWord == anipang[k][i])
+				{
+					arr[0]++;
+					arr[1]++;
+				}
+				else if (basicWord == anipang[k][i + 1] || basicWord == anipang[k][i - 1])
+				{
+					arr[1] = arr[0] + 1;
+					arr[0] = 0;
+				}
+				else if (basicWord == anipang[k + 1][i] && anipang[k + 1][i] == anipang[k - 1][i])
+				{
+					arr[1] = arr[0] + 1;
+					M = max({ M,arr[0], arr[1] });
+					arr[1] = 1;
+					arr[0] = 0;
+				}
+				else
+				{
+					arr[1] = 0;
+					arr[0] = 0;
+				}
 
-    return 0;
+				M = max({ M,arr[0], arr[1] });
+			}
+		}
+	}
+
+	cout << M;
+
+
+	return 0;
 }
