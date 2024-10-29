76ed94f9d11ccbdcf5749213d8a87b529f9fec20
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-10-29 11:03:47 +0900

    [Silver III] Title: 칸토어 집합, Time: 4 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -2,40 +2,38 @@
 #include <string>
 using namespace std;
 
-// 재귀적으로 칸토어 집합을 구성하는 함수
-void CantorSet(string& str, int start, int length) {
-    if (length == 1) {
-        return; // 길이가 1이면 더 이상 처리할 필요가 없음
+void GenerateCantorSet(string &Line, int Start, int Length)
+{
+    if (Length == 1)
+    {
+        return;
     }
+
+    int SectionLength = Length / 3;
     
-    int divide = length / 3;
-
-    // 가운데 구간을 공백으로 설정
-    for (int i = start + divide; i < start + 2 * divide; ++i) {
-        str[i] = ' ';
+    for (int i = Start + SectionLength; i < Start + 2 * SectionLength; i++)
+    {
+        Line[i] = ' ';
     }
 
-    // 왼쪽 부분과 오른쪽 부분에 대해 재귀적으로 함수 호출
-    CantorSet(str, start, divide); // 왼쪽
-    CantorSet(str, start + 2 * divide, divide); // 오른쪽
+    GenerateCantorSet(Line, Start, SectionLength);
+    GenerateCantorSet(Line, Start + 2 * SectionLength, SectionLength);
 }
 
-int main() {
+int main()
+{
     int N;
-    while (cin >> N) {
-        int length = 1;
-        for (int i = 0; i < N; ++i) {
-            length *= 3;
+    while (cin >> N)
+    {
+        int Length = 1;
+        for (int i = 0; i < N; i++)
+        {
+            Length *= 3;
         }
 
-        // 길이가 length인 문자열을 '-'로 초기화
-        string str(length, '-');
-
-        // 칸토어 집합 생성
-        CantorSet(str, 0, length);
-
-        // 결과 출력
-        cout << str << endl;
+        string Line(Length, '-');
+        GenerateCantorSet(Line, 0, Length);
+        cout << Line << endl;
     }
 
     return 0;
