30642b2c71a6babaf79ca56d4bac52f13a559259
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-11-06 11:20:27 +0900

    [Bronze III] Title: 부호, Time: 304 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.cc
+++ 백준/Bronze/1247. 부호/부호.cc
@@ -1,56 +1,49 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 
+int main()
+{
+    for (int T = 0; T < 3; ++T)
+    {
+        int N = 0;
+        cin >> N;
 
-/* 부호 https://www.acmicpc.net/problem/1247
-if 문에서 overflow 발생 시 undefined behavior에 해당되 자동으로 false 처리
-LONG_MAX, LONG_MIN 이용해서 overflow를 안 일으키고 검사하는 것이 포인트
-https://www.acmicpc.net/board/view/56858 */
+        long long Sum = 0, Flow = 0, Input = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            cin >> Input;
 
+            long long Temp = Sum;
+            Sum += Input;
 
-int main(void){
-    long long result=0, overflow=0;
-    int n;
-
-    for (int test_case=0; test_case<3; test_case++){
-        std::cin>>n;
-        
-        result   = 0;
-        overflow = 0;
-
-        for (int i=0; i < n; i++){
-            long long tmp;
-            std::cin>>tmp;
-
-            if (result > 0 and tmp > 0 and tmp > LLONG_MAX - result) {
-                //result = result + tmp - LLONG_MIN;
-                overflow += 1;
+            // 양수 && 양수 && 음수 → 오버플로우
+            if (Input > 0 && Temp > 0 && Sum < 0)
+            {
+                Flow++;
             }
-
-            if (result < 0 and tmp < 0 and tmp < LONG_MIN - result) {
-                //result = result + tmp - LLONG_MAX;
-                overflow -= 1;
+            // 음수 && 음수 && 양수 → 언더플로우
+            if (Input < 0 && Temp < 0 && Sum > 0)
+            {
+                Flow--;
             }
-
-            result += tmp;
         }
 
-        if (overflow < 0){
-            std::cout<<"-\n";
+        if (Flow == 0)
+        {
+            if (Sum == 0)
+            {
+                cout << '0' << '\n';
+            }
+            else
+            {
+                cout << (Sum > 0 ? '+' : '-') << '\n';
+            }
         }
-        else if (overflow > 0){
-            std::cout<<"+\n";
-        }
-        else if (result==0){
-            std::cout<<"0\n";
-        }
-        else if (result < 0){
-            std::cout<<"-\n";
-        }
-        else if (result > 0){
-            std::cout<<"+\n";
+        else
+        {
+            cout << (Flow > 0 ? '+' : '-') << '\n';
         }
     }
 
+    return 0;
 }
