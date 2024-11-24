69f6c88d702d025e09b9872968a324fd9a2e9ecd
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-11-24 23:41:21 +0900

    [Bronze I] Title: 나무 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int wood[5]{ 0 };
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> wood[i];
+    }
+
+	while (true) 
+    {
+		for (int j = 0; j < 4; j++) 
+        {
+			if (wood[j] > wood[j + 1]) 
+            {
+				swap(wood[j], wood[j+1]);
+				for (auto i : wood)
+                {
+                    cout << i << ' ';
+                }
+				cout << endl;
+			}
+		}
+        
+		if (wood[0] == 1 && wood[1] == 2 && wood[2] == 3 && wood[3] == 4 && wood[4] == 5)
+        {
+             break;
+        }
+	}
+    
+    return 0;
+}
