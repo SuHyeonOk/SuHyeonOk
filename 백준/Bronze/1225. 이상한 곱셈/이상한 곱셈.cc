a67b7ab64fa0e0a0e757452fca3c41ce7086f039
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-11-27 22:02:44 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 72 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	string a = "", b = "";
+	cin >> a >> b;
+    
+    long long sum = 0;
+	int alen = a.length();
+	int blen = b.length();
+	for (int i = 0; i < alen; i++)
+	{
+		for (int j = 0; j < blen; j++)
+        {
+            sum += (int)(a[i] - 48) * (int)(b[j] - 48);
+        }
+	}
+	cout << sum;
+    
+	return 0;
+}
