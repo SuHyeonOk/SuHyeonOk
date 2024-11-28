87f611f13f8d2a52b53ab2b4f7898e94c949fee5
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-11-28 23:32:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+	string str;
+	cin >> str;
+
+	string fan = ":fan:";
+
+	cout << fan << fan << fan<<"\n";
+	cout << fan << ":" << str << ":" << fan << "\n";
+	cout << fan << fan << fan;
+
+    return 0;
+}
