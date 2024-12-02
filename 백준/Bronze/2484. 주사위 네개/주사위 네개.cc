3ae56e4b97e48cc8fe41e7682f10017d37d02de8
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2024-12-02 23:48:11 +0900

    [Bronze II] Title: 주사위 네개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n = 0, total =0;
+	cin >> n;
+	while (n--) 
+    {
+		int arr[7]{ 0 };
+		
+		for (int i = 0; i < 4; i++) 
+        {
+			int tmp;
+			cin >> tmp;
+			arr[tmp]++;
+		}
+
+		for (int j = 6; j > 0; j--) 
+        {
+			if (arr[j] == 4) 
+            {
+				total = total < 50000 + 5000 * j ? 50000 + 5000 * j : total;
+				break;
+			}
+			else if (arr[j] == 3) 
+            {
+				total = total < 10000 + 1000 * j ? 10000 + 1000 * j : total;
+				break;
+			}
+			else if (arr[j] == 2)
+            {
+				for (int k = j - 1; k > 0; k--)
+                {
+					if (arr[k] == 2) 
+                    {
+						total = total < 2000 + j * 500 + k * 500 ? 2000 + j * 500 + k * 500 : total;
+						break;
+					}
+					else 
+                    {
+						total = total < 1000 + 100 * j ? 1000 + 100 * j : total;
+					}
+				}
+				break;
+			}
+			else if(arr[j] == 1) 
+            {
+				total = total < 100 * j ? 100 * j : total;
+			}
+		}
+	}
+
+	cout << total;
+    return 0;
+}
